16988733595c520cd0e08fe57abe12e1c232fd6c
Wama85 <wi_an9@hotmail.com>
2024-11-27 20:54:17 -0400

    File integration for common code

--- features/step_definitions/resetForm.rb
+++ features/step_definitions/resetForm.rb
@@ -1,35 +1,4 @@
-Given(/^I am on the GMO Home Page Reset$/) do
-    page.driver.browser.m
   
-  And(/^I click on the "Enter GMO OnLine" button for resetting$/) do
-    click_button('Enter GMO OnLine')
-  end
-  
-  When(/^I fill the form with the following quantities:$/) do |table|
-    table.raw.each do |row|
-      product_name = row[0]
-      quantity = row[1]
-  
-      case product_name
-      when "3 Person Dome Tent"
-        fill_in 'QTY_TENTS', with: quantity
-      when "External Frame Backpack"
-        fill_in 'QTY_BACKPACKS', with: quantity
-      when "Glacier Sun Glasses"
-        fill_in 'QTY_GLASSES', with: quantity
-      when "Padded Socks"
-        fill_in 'QTY_SOCKS', with: quantity
-      when "Hiking Boots"
-        fill_in 'QTY_BOOTS', with: quantity
-      when "Back Country Shorts"
-        fill_in 'QTY_SHORTS', with: quantity
-      end
-    end
-  end
-  
-  When(/^I click on the "Reset Form" button for resetting$/) do
-    click_button('Reset Form')
-  end
   
   Then(/^all fields should be cleared$/) do
     expect(page.find('input[name="QTY_TENTS"]').value).to eq("0")
