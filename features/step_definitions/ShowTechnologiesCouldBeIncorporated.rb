2fcef170bed00c275430a82c000fbc8cb6a22dff
978sebasBeller <sebastian.beller@ucb.edu.bo>
2024-11-29 12:56:45 -0400

    Merge conflicts have been solved

--- features/step_definitions/ShowTechnologiesCouldBeIncorporated.rb
+++ features/step_definitions/ShowTechnologiesCouldBeIncorporated.rb
@@ -2,10 +2,6 @@ Given('I am at the GMO online site for future technologies') do
     visit 'https://demo.borland.com/gmopost/'
   end
   
-  When('I click the "About the GMO Site" button') do
-    find(:xpath, '/html/body/form/div[1]/center/table/tbody/tr/td[2]/input').click
-  end
-  
   Then('I see the list of future technologies for the site') do |table|
     expected_technologies = table.raw.flatten
     expected_technologies.each do |technology|
